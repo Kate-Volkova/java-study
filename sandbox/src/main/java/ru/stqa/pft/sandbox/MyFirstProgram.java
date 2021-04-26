09c2a032de711522a7908ae8f0d80a12c1538bf8
Kate Volkova <let.kate.test@gmail.com>
2021-04-26 11:48:45 +0200

    Functions added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,24 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World!");
+		hello("Katia");
+		hello("tester");
+		int a = 2;
+		System.out.println("Площадь квадрата со стороной " + a + " равна " + square(a));
+		int c = 2; int d = 5;
+		System.out.println("Площадь прямоугольника со сторонами " + c + " и " + d + " равна " + square(c, d));
+	}
+
+	public static void hello (String name) {
+		System.out.print("Hello, " + name + "! ");
+	}
+
+	public static int square (int a) {
+		return a * a;
+	}
+
+	public static int square (int a, int b) {
+		return a * b;
 	}
 
 }
